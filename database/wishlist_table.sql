32641b1037e46e2519e8232a8f666bc195ad72dd
Youcef Kesbi <ykesbi@gmail.com>
2025-08-27 09:45:49 +0100

    .

--- database/wishlist_table.sql
+++ database/wishlist_table.sql
@@ -1,5 +1,5 @@
 -- Wishlist table structure for Supabase
--- This table stores user wishlist items
+-- This table stores user wishlist items -
 
 CREATE TABLE IF NOT EXISTS public.wishlist (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
