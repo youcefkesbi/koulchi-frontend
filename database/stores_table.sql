bbc77c42da8cc777f33ab62d357f0e19176f24c2
Youcef Kesbi <ykesbi@gmail.com>
2025-09-02 20:38:38 +0100

    Add row-level security policies for store management
    - Implemented policies allowing authenticated users to insert, select, update, and delete their own stores in the stores_table.sql.
    - Added a policy to allow public access for viewing all stores, enhancing data visibility while maintaining user-specific access control.

--- database/stores_table.sql
+++ database/stores_table.sql
@@ -13,3 +13,35 @@ CREATE TABLE public.stores (
 
   CONSTRAINT unique_owner UNIQUE(owner_id) -- optional: one store per user
 );
+
+
+create policy "users can insert their own store"
+on stores
+for insert
+to authenticated
+with check (owner_id = auth.uid());
+
+create policy "users can select their own store"
+on stores
+for select
+to authenticated
+using (owner_id = auth.uid());
+
+create policy "users can update their own store"
+on stores
+for update
+to authenticated
+using (user_id = auth.uid())
+with check (owner_id = auth.uid());
+
+create policy "users can delete their own store"
+on stores
+for delete
+to authenticated
+using (owner_id = auth.uid());
+
+create policy "everyone can view stores"
+on stores
+for select
+to public
+using (true);  -- all rows are visible
