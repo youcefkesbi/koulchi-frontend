3f4c8d6e00aef28bdfa0b1c7ab3ad8fb48e058bd
youcef <ykesbi@gmail.com>
2026-01-17 01:03:45 +0300

    feat: Add trigger for handling new user creation in auth.users table
    - Created a new trigger that executes the handle_new_user function after a new user is inserted into the auth.users table, enhancing user role management.

--- database/user_roles_table.sql
+++ database/user_roles_table.sql
@@ -220,3 +220,8 @@ begin
   return new;
 end;
 $$ language plpgsql security definer;
+
+
+create trigger on_auth_user_created
+after insert on auth.users
+for each row execute function public.handle_new_user();
